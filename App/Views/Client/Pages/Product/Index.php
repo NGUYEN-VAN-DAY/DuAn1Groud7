d95a29e26365f380c9c282285a4de413644dc5a9
NGUYEN-VAN-DAY <daynvpc08855@gmail.com>
2024-11-18 13:55:35 +0700

    [html] giao diện trang sản phẩm

--- App/Views/Client/Pages/Product/Index.php
+++ App/Views/Client/Pages/Product/Index.php
@@ -12,40 +12,13 @@ class Index extends BaseView
 
 
 ?>
-        <section class="shop_section layout_padding">
-            <div class="row">
-                <div class="col-md-3">
-                    <?php
-                    Category::render($data['categories']);
-                    ?>
-                </div>
-                <div class="col-md-9">
-                    <?php
-                    if (count($data) && count($data['products'])) :
-                    ?>
-                        <div class="container">
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
+        <div class="container-fluid">
+            <section class="shop_section layout_padding">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div>
 
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <h3>
-                                        Sản phẩm
-                                        </h2>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="dropdown">
-                                        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            Dropdown button
-                                        </button>
-                                        <ul class="dropdown-menu">
-                                            <li><a class="dropdown-item" href="#">Action</a></li>
-                                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
                             <?php
                             Category::render($data['categories']);
                             ?>
@@ -64,27 +37,22 @@ class Index extends BaseView
                                         </h3>
                                     </div>
                                     <div class="col-md-3 text-right  ">
-                                        <label for="fruits">Sắp xếp theo mặc định:</label>
-                                        <select id="choose" name="choose" onchange="" class="border-0 form-select-sm bg-light  " form="fruitform">
-                                            <option value="ttc" >Từ thấp tới cao</option>
-                                            <option value="ctt" >Từ cao tới thấp</option>
-                                            <option value="opel">Sản phẩm nổi bật</option>
-                                            <option value="opel">Mới nhất</option>
-                                            <option value="opel">Cũ nhất</option>
-
-                                        </select>
-                                        <p id="output"></p>
+                                    <label for="fruits">Default Sorting:</label>
+                                    <select id="fruits" name="fruitlist" class="border-0 form-select-sm bg-light  " form="fruitform">
+                                        <option value="volvo">Nothing</option>
+                                        <option value="saab">Popularity</option>
+                                        <option value="opel">Organic</option>
+                                        <option value="audi">Fantastic</option>
+                                    </select>
                                     </div>
                                 </div>
                             </div>
-                            <div class="row" >
-                              
+                            <div class="row">
                                 <?php
                                 foreach ($data['products'] as $item) :
                                 ?>
-                                    <div class="col-sm-6 col-md-4 col-lg-3"id="product" >
-
-                                        <div class="box" >
+                                    <div class="col-sm-6 col-md-4 col-lg-3">
+                                        <div class="box">
                                             <a href="/products/<?= $item['id'] ?>" class="">
                                                 <div class="card mb-4 shadow-sm">
                                                     <img class="img-index" src="<?= APP_URL ?>/public/uploads/products/<?= $item['image'] ?>" class="card-img-top" alt="" style="width: 100%;  display: block;" data-holder-rendered="true">
@@ -99,9 +67,6 @@ class Index extends BaseView
                                                         ?>
                                                             <p>Giá gốc: <strike><?= number_format($item['price']) ?> đ</strike></p>
                                                             <p>Giảm giá: <strong class="text-danger"><?= number_format($item['price'] - $item['discount_price']) ?> đ</strong></p>
-                                                            <p style="display: none;" id="priceProduct"> <?= $item['price'] - $item['discount_price'] ?> </p>
-
-                                                            <!-- <?php echo $data['product']['price'] - $data['product']['discount_price'] ?> -->
                                                         <?php
                                                         else :
                                                         ?>
@@ -126,7 +91,6 @@ class Index extends BaseView
 
                                 ?>
                             </div>
-                        
                         <?php
                         else :
                         ?>
@@ -136,7 +100,6 @@ class Index extends BaseView
                         ?>
                     </div>
                 </div>
-            
         </div>
         </section>
 
